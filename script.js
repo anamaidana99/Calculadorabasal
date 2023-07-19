00657a59d46efdceeb12a84e99519264f856eeb1
anamaidana99 <139254226+anamaidana99@users.noreply.github.com>
2023-07-18 20:14:51 -0400

    Add files via upload
    corrected js code

--- script.js
+++ script.js
@@ -5,79 +5,64 @@ const INPUT = document.getElementById("peso");
 const MAN = document.getElementById('man');
 
 BOTON.addEventListener("click", () => {
-    const DATO = document.getElementById("peso").value
-    if (DATO > 0 && DATO !== " "){
-        ERROR.style.display = "none"
-        if (DATO <= 30){
-        //holliday    
+    const DATO = parseFloat(document.getElementById("peso").value);
+    if (DATO > 0 && !isNaN(DATO)) {
+        ERROR.style.display = "none";
+        if (DATO <= 30) {
+            // Holliday
             let flujo = holliday(DATO);
-            let mantenimiento = ((flujo/24) * 1.5).toFixed(2);
-            FLUJO.innerHTML = "Volumen diario: " + flujo + " cc";
-            MAN.innerHTML = "m+m/2: " + mantenimiento + " cc/hr";
+            let mantenimiento = (flujo / 24 * 1.5).toFixed(2);
+            FLUJO.textContent = "Volumen diario: " + flujo + " cc";
+            MAN.textContent = "m+m/2: " + mantenimiento + " cc/hr";
             FLUJO.style.display = "block";
             MAN.style.display = "block";
         } else {
-        //superficie corporal 
+            // Supercorporal
             let flujo = supCorporal(DATO);
-            let mantenimiento = ((flujo/24) * 1.5).toFixed(2);
-            FLUJO.innerHTML = "Volumen diario: " + supCorporal(DATO) + " cc";
-            MAN.innerHTML = "m+m/2: " + mantenimiento + " cc/hr";
+            let mantenimiento = (flujo[0] / 24 * 1.5).toFixed(2);
+            FLUJO.textContent = "Volumen diario: " + flujo[0] + " - " + flujo[1] + " cc";
+            MAN.textContent = "m+m/2: " + mantenimiento + " cc/hr";
             FLUJO.style.display = "block";
             MAN.style.display = "block";
         }
-    }else{
+    } else {
         ERROR.style.display = 'block';
         FLUJO.style.display = 'none';
         MAN.style.display = 'none';
-    };    
+    }
 });
 
-function holliday(peso){
-let flujo = 0;
-if (peso <= 10){
-    flujo = peso*100;
-} else if (peso > 10 && peso <= 20){
-    flujo = (1000 + (peso - 10) * 50);
-} else if (peso > 20){
-    flujo = (1500 + (peso - 20) * 20);
-}
+function holliday(peso) {
+    let flujo = 0;
+    if (peso <= 10) {
+        flujo = peso * 100;
+    } else if (peso > 10 && peso <= 20) {
+        flujo = 1000 + (peso - 10) * 50;
+    } else if (peso > 20) {
+        flujo = 1500 + (peso - 20) * 20;
+    }
     return flujo.toFixed(2);
 }
 
-function supCorporal(peso){
-    let flujo = ( (peso * 4) + 7) / (peso + 90)
-    return flujo.toFixed(2);
-}
-
-//contender
-//var open_dd = false;	
-function triggercontent(element) {
-    //alert("lauf " +open_dd);
-    var element_dd = element.nextElementSibling;
-    
-    while(element_dd && element_dd.tagName == "DD") {
-    
-    if ((element_dd.style.display == 'none') || ! element_dd.style.display)
-    element_dd.style.display = 'block';
-    else
-    element_dd.style.display = 'none';
-    
-    //alert(element_dd);
-    element_dd = element_dd.nextElementSibling;
-    }
-
-    /*open_dd = true;
-    
-    while(element_dd && element_dd.tagName == "DD" && open_dd == true) {
-    
-    if (element_dd.style.display = "block")
-    element_dd.style.display = "none";
-    
-    element_dd = element_dd.nextElementSibling;
-    }
-    open_dd = false;*/
-    
+function supCorporal(peso) {
+    let supeficieCorporal = (peso * 4 + 7) / (peso + 90);
+    return [(supeficieCorporal * 1500).toFixed(2), (supeficieCorporal * 2000).toFixed(2)];
 }
 
 
-
+// //contender
+// //var open_dd = false;	
+// function triggercontent(element) {
+//     //alert("lauf " +open_dd);
+//     var element_dd = element.nextElementSibling;
+    
+//     while(element_dd && element_dd.tagName == "DD") {
+    
+//     if ((element_dd.style.display == 'none') || ! element_dd.style.display)
+//     element_dd.style.display = 'block';
+//     else
+//     element_dd.style.display = 'none';
+    
+//     //alert(element_dd);
+//     element_dd = element_dd.nextElementSibling;
+//     }
